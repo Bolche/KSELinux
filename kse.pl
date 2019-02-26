b9bffbc1e578fec34dbc851e8b6c3e169ce674f7
Kaleb Klein <klein.jae@gmail.com>
2019-02-25 21:22:55 -0500

    Version bump
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- kse.pl
+++ kse.pl
@@ -76,7 +76,7 @@ use Tk::TList;
 use Tk::ItemStyle;
 # use Tk::ErrorDialog;
 require Tk::Dialog;
-our $version='v3.3.7a-20190225';
+our $version='v3.3.7d';
 if ($Tk::VERSION  eq '800.029') { $version .= ' alternate'}
 use Win32::FileOp;
 use Win32::TieRegistry;
